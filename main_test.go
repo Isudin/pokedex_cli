86198db2eabef996c079d9dda83eb45a0250ea0f
Isu <ntrojak98@gmail.com>
2025-01-10 19:58:37 +0100

    [RED] Added TestCleanInput

--- main_test.go
+++ main_test.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"strings"
 	"testing"
 )
 
-func TestCleanInput(t *testing.T) []string {
-	return []string{}
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "",
+			expected: []string{},
+		},
+		{
+			input:    "-- 12Test. aaa_$!",
+			expected: []string{"--", "12Test.", "aaa_$!"},
+		},
+	}
+
+	for _, c := range cases {
+		output := cleanInput(c.input)
+		allOutput := strings.Join(output, ", ")
+		allExpected := strings.Join(c.expected, ", ")
+		if len(output) != len(c.expected) {
+			t.Errorf("Error matching output\nExpected: [%v]\nOutput: [%v]", allExpected, allOutput)
+		}
+		for i := range output {
+			if output[i] != c.expected[i] {
+				t.Errorf("Error matching output\nExpected: [%v]\nOutput: [%v]", allExpected, allOutput)
+			}
+		}
+	}
 }
