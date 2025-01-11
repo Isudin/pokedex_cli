c518f19d8bbfe71dc846a90b53e38d7032296d8a
Isu <ntrojak98@gmail.com>
2025-01-11 11:28:34 +0100

    added basics of commands mapping

--- main.go
+++ main.go
@@ -23,7 +23,29 @@ func main() {
 	}
 }
 
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
 func cleanInput(text string) []string {
 	lowerString := strings.ToLower(text)
 	return strings.Fields(lowerString)
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
 }
